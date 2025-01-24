cd4f5cf722ff23d7db2432c1b53e59d24a25c163
Seerat Jaan <meersuhana266@gmail.com>
2025-01-24 13:41:02 +0530

    removed unnecessary code

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Javascript
-# Javascript
-# Javascript
---
+++ index.js
@@ -0,0 +1,6 @@
+const myName = "Nasyx Rakeeb";
+const myAge = 22;
+const myProfession = "Dev";
+const myAddress = "JK";
+
+console.log(myAddress);
